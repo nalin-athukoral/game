fc8d1a99a97e6507bf55615aca3cbfd8f4c07ef3
indrajithathukorala <nalinathukoralanalin@gmail.com>
2022-10-22 21:39:27 +0530

    Hello typescript 2

---
+++ home/student.ts
@@ -0,0 +1,17 @@
+let num1:number;
+let num2:number;
+
+export function A() {
+    console.log(num1+num2);
+    
+}
+
+export function B() {
+    console.log('log');
+    
+}
+
+export function C() {
+    console.log("dsdads");
+    
+}
